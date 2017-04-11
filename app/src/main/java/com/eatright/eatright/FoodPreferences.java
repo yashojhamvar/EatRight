b6c13582b01fc92398ba2007554befdb6a522f81
yashojhamvar <yasho170@gmail.com>
2017-04-10 20:04:51 -0400

    With FoodPreferences.java

--- app/src/main/java/com/eatright/eatright/FoodPreferences.java
+++ app/src/main/java/com/eatright/eatright/FoodPreferences.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,12 +25,13 @@ public class FoodPreferences extends AppCompatActivity {
     ArrayList<String> avoidVeg_list = new ArrayList<String>();
     ArrayList<String> avoidMeat_list = new ArrayList<String>();
     ArrayList<String> conditions_list = new ArrayList<String>();
+    ArrayList<String> diets_list = new ArrayList<String>();
+    ArrayList<String> calorie_list = new ArrayList<String>();
 
-    //private SQLiteDatabase sqLiteDatabase;
     MyDBHandler dbHandler;
-    //private Cursor cursor;
     public static String USERNAME;
     public ArrayList<String> dbRecords;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +42,13 @@ public class FoodPreferences extends AppCompatActivity {
         USERNAME = getIntent().getExtras().getString("username");
         Toast.makeText(this, "UserName is : " + USERNAME, Toast.LENGTH_SHORT).show();
 
-        /* *******************************************************************
+       /*  *******************************************************************
         *                       Query and Show records on page load
         * ******************************************************************* */
         dbHandler = new MyDBHandler(this, null, null, 1);
-        /*SQLiteDatabase sqLiteDatabase = dbHandler.getReadableDatabase();
-        sqLiteDatabase = openOrCreateDatabase("eatRightDB", Context.MODE_PRIVATE, null);
-        int dbVersion = sqLiteDatabase.getVersion();*/
         Toast.makeText(this, "before retrieveData", Toast.LENGTH_LONG).show();
         retrieveData();
         setCheckboxState();
-
 
         //(Context, String Name,CursorFactory factory,int version)
         dbHandler.close();
@@ -77,7 +75,6 @@ public class FoodPreferences extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Retrieved data = NULL", Toast.LENGTH_LONG).show();
         }
-
     }
 
     /* *******************************************************************
@@ -88,26 +85,103 @@ public class FoodPreferences extends AppCompatActivity {
     * Date: 4/9/2017
     * ******************************************************************* */
     public void setCheckboxState() {
+        CheckBox mealType_nonveg = (CheckBox) findViewById(R.id.mealType_nonveg);
+        CheckBox mealType_veg = (CheckBox) findViewById(R.id.mealType_veg);
+        CheckBox mealType_vegan = (CheckBox) findViewById(R.id.mealType_vegan);
+        CheckBox avoidMeat_chicken = (CheckBox) findViewById(R.id.avoidMeat_chicken);
+        CheckBox avoidMeat_pork = (CheckBox) findViewById(R.id.avoidMeat_pork);
+        CheckBox avoidMeat_fish = (CheckBox) findViewById(R.id.avoidMeat_fish);
+        CheckBox avoidMeat_beef = (CheckBox) findViewById(R.id.avoidMeat_beef);
+        CheckBox avoidVeg_onions = (CheckBox) findViewById(R.id.avoidVeg_onions);
+        CheckBox avoidVeg_corn = (CheckBox) findViewById(R.id.avoidVeg_corn);
+        CheckBox avoidVeg_lettuce = (CheckBox) findViewById(R.id.avoidVeg_lettuce);
+        CheckBox avoidVeg_tomato = (CheckBox) findViewById(R.id.avoidVeg_tomato);
+        CheckBox avoidVeg_diary = (CheckBox) findViewById(R.id.avoidVeg_diary);
+        CheckBox avoidVeg_wheat = (CheckBox) findViewById(R.id.avoidVeg_wheat);
+        CheckBox userConditions_diabetes = (CheckBox) findViewById(R.id.userConditions_diabetes);
+        CheckBox userConditions_lactoseIntolerance = (CheckBox) findViewById(R.id.userConditions_lactoseIntolerance);
+        CheckBox userConditions_glutenIntolerance = (CheckBox) findViewById(R.id.userConditions_glutenIntolerance);
+        CheckBox userConditions_nutAllergy = (CheckBox) findViewById(R.id.userConditions_nutAllergy);
+        CheckBox userConditions_highBloodPressure = (CheckBox) findViewById(R.id.userConditions_highBloodPressure);
+        CheckBox diet_keto = (CheckBox) findViewById(R.id.diet_keto);
+        CheckBox diet_helal = (CheckBox) findViewById(R.id.diet_helal);
+
+
         ArrayList<String> fetchedRecords = dbRecords;
-        for (String record : fetchedRecords) {
+        for (
+                String record : fetchedRecords)
+
+        {
             String[] separated = record.split(",");
             for (String item : separated) {
-                if (item.equalsIgnoreCase("Vegetarian")) {
-
+                if (item.equalsIgnoreCase("Non-Vegetarian Meals")) {
+                    mealType_nonveg.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Vegetarian Meals")) {
+                    mealType_veg.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Vegan Meals")) {
+                    mealType_vegan.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Chicken")) {
+                    avoidMeat_chicken.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Pork")) {
+                    avoidMeat_pork.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Fish")) {
+                    avoidMeat_fish.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Beef")) {
+                    avoidMeat_beef.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Onions")) {
+                    avoidVeg_onions.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Corn")) {
+                    avoidVeg_corn.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Tomatoes")) {
+                    avoidVeg_tomato.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Dairy")) {
+                    avoidVeg_diary.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Wheat")) {
+                    avoidVeg_wheat.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Diabetes")) {
+                    userConditions_diabetes.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Lactose Intolerance")) {
+                    userConditions_lactoseIntolerance.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Gluten Intolerance")) {
+                    userConditions_glutenIntolerance.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Nut Allergy")) {
+                    userConditions_nutAllergy.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("High Blood Pressure")) {
+                    userConditions_highBloodPressure.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Ketogenic Diet")) {
+                    diet_keto.setEnabled(true);
+                }
+                if (item.equalsIgnoreCase("Dr. Sumi Helal Diet")) {
+                    diet_helal.setEnabled(true);
                 }
             }
-            //separated[0];
-            //separated[1];
         }
     }
 
-    /* *******************************************************************
-    * Parameters: view state
-    * Return: none
-    * Description: Populates lists with selected Checkboxes
-    * Author: Srishti Hunjan
-    * Date: 4/9/2017
-    * ******************************************************************* */
+    /*  *******************************************************************
+     * Parameters: view state
+     * Return: none
+     * Description: Populates lists with selected Checkboxes
+     * Author: Srishti Hunjan
+     * Date: 4/9/2017
+     * ********************************************************************/
     public void selectItem(View view) {
         boolean checked = ((CheckBox) view).isChecked();
         switch (view.getId()) {
@@ -153,9 +227,21 @@ public class FoodPreferences extends AppCompatActivity {
                 break;
             case R.id.avoidVeg_tomato:
                 if (checked)
-                    avoidVeg_list.add("Tomato");
+                    avoidVeg_list.add("Tomatoes");
                 else
-                    avoidVeg_list.remove("Tomato");
+                    avoidVeg_list.remove("Tomatoes");
+                break;
+            case R.id.avoidVeg_diary:
+                if (checked)
+                    avoidVeg_list.add("Dairy");
+                else
+                    avoidVeg_list.remove("Dairy");
+                break;
+            case R.id.avoidVeg_wheat:
+                if (checked)
+                    avoidVeg_list.add("Wheat");
+                else
+                    avoidVeg_list.remove("Wheat");
                 break;
 
             // Avoid which Meats
@@ -178,6 +264,12 @@ public class FoodPreferences extends AppCompatActivity {
                 else
                     avoidMeat_list.remove("Pork");
                 break;
+            case R.id.avoidMeat_beef:
+                if (checked)
+                    avoidMeat_list.add("Beef");
+                else
+                    avoidMeat_list.remove("Beef");
+                break;
 
             // User Conditions to consider
 
@@ -199,12 +291,35 @@ public class FoodPreferences extends AppCompatActivity {
                 else
                     conditions_list.remove("Lactose Intolerance");
                 break;
+            case R.id.userConditions_nutAllergy:
+                if (checked)
+                    conditions_list.add("Nut Allergy");
+                else
+                    conditions_list.remove("Nut Allergy");
+                break;
+            case R.id.userConditions_highBloodPressure:
+                if (checked)
+                    conditions_list.add("High Blood Pressure");
+                else
+                    conditions_list.remove("High Blood Pressure");
+                break;
         }
     }
 
     public void savePreferences(View view) {
-        //(String username, String meal_type, String avoidVeg, String avoidMeat, String conditions)
-        UserPreferences userPreferences = new UserPreferences(USERNAME, mealType_list.toString(), avoidVeg_list.toString(), avoidMeat_list.toString(), conditions_list.toString());
+        EditText constraint_Protein = (EditText) findViewById(R.id.constraint_Protein);
+        final EditText constraint_carbs = (EditText) findViewById(R.id.constraint_carbs);
+        final EditText constraint_sugar = (EditText) findViewById(R.id.constraint_sugar);
+        final EditText constraint_calories = (EditText) findViewById(R.id.constraint_calories);
+        final EditText constraint_fat = (EditText) findViewById(R.id.constraint_fat);
+
+        //(String username, String meal_type, String avoidVeg, String avoidMeat, String conditions, String diets)
+        calorie_list.add(constraint_Protein.toString());
+        calorie_list.add(constraint_carbs.toString());
+        calorie_list.add(constraint_fat.toString());
+        calorie_list.add(constraint_calories.toString());
+        calorie_list.add(constraint_sugar.toString());
+        UserPreferences userPreferences = new UserPreferences(USERNAME, mealType_list.toString(), avoidVeg_list.toString(), avoidMeat_list.toString(), conditions_list.toString(), diets_list.toString(), calorie_list.toString());
         int recordCount = dbHandler.addPreferences(userPreferences);
         String testStr = "Preferences Saved!! : " + String.valueOf(recordCount);
         Toast.makeText(this, testStr, Toast.LENGTH_LONG).show();
@@ -212,4 +327,3 @@ public class FoodPreferences extends AppCompatActivity {
         startActivity(mainPage);
     }
 }
-
